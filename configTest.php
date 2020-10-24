07b5132b3528bd2c867854ba0f9ea6af2461a3d2
TotallyAProgrammer <zane.reick@unknownts.tk>
2020-10-24 20:22:53 +0000

    configTest is now much more fleshed out for easier configuration teting. Updated rjwt.ini.php to have a testing username and password, and fixed a type in gitignore

--- configTest.php
+++ configTest.php
@@ -8,3 +8,20 @@ echo $rjwtConfig["secret"] . "\n";
 echo $rjwtConfig["nasIPID"] . "\n";
 echo $rjwtConfig["nasID"] . "\n";
 echo $rjwtConfig["keyFile"] . "\n";
+
+require_once './radius/autoload.php';
+use Dapphp\Radius\Radius;
+
+$client = new Radius();
+$client->setServer($rjwtConfig["serverIP"])
+	->setSecret($rjwtConfig["secret"])
+	->setNasIpAddress($rjwtConfig["nasIPID"])
+	->setAttribute(32, $rjwtConfig["nasID"]);
+
+$authenticated = $client->accessRequest($rjwtConfig["testingUsername"], $rjwtConfig["testingPassword"]);
+
+if ($authenticated === false) {
+	sprintf("Access-Request failed with error %d (%s).\n", $client->getErrorMessaged());
+} else {
+	print("Configuration testing passed!");
+}
