b02b81807cab7ba6dc4502f8897da3750cebbd0b
Zane D. R <kyle.unknown@outlook.com>
2020-10-27 11:47:00 -0500

    Changed \n to <br />, added security conf to configTest

--- configTest.php
+++ configTest.php
@@ -1,13 +1,17 @@
 <?php
 $configLocation = "rjwt.ini.php";
+$secConfLocation = "security_conf.ini.php";
 
-$rjwtConfig = parse_ini_file($configLocation) or die("Could not read config");
+$rjwtConfig = parse_ini_file($configLocation) or die("Could not read RJWT config");
+print("RJWT Config read<br />");
+$SecConfig  = parse_ini_file($secConfLocation) or die("Could not read Security config");
+print("Security Config read<br />");
 
-echo $rjwtConfig["serverIP"] . "\n";
-echo $rjwtConfig["secret"] . "\n";
-echo $rjwtConfig["nasIPID"] . "\n";
-echo $rjwtConfig["nasID"] . "\n";
-echo $rjwtConfig["keyFile"] . "\n";
+print($rjwtConfig["serverIP"] . "<br />");
+print($rjwtConfig["secret"] . "<br />");
+print($rjwtConfig["nasIPID"] . "<br />");
+print($rjwtConfig["nasID"] . "<br />");
+print($rjwtConfig["keyFile"] . "<br />");
 
 require_once './radius/autoload.php';
 use Dapphp\Radius\Radius;
@@ -21,7 +25,7 @@ $client->setServer($rjwtConfig["serverIP"])
 $authenticated = $client->accessRequest($rjwtConfig["testingUsername"], $rjwtConfig["testingPassword"]);
 
 if ($authenticated === false) {
-	sprintf("Access-Request failed with error %d (%s).\n", $client->getErrorMessaged());
+	sprintf("Access-Request failed with error %d (%s).<br />", $client->getErrorMessaged());
 } else {
 	print("Configuration testing passed!");
 }
